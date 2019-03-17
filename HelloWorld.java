e0891d3343aec1e75f279a00d5528444e4464f0a
s-nandakumar-in <48184106+s-nandakumar-in@users.noreply.github.com>
2019-03-17 09:39:55 +0530

    print statement comment included

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 // This is a sample code
   public static void main (String arg[]) {
     System.out.println ("Hello World..!!..");
+    //The above is a print statement and this is for its comment
   }
 
 }
